9a288cebfcd7c7c931fa76f83da473c9fcdf15c2
Omar Moreno <omoreno1@ucsc.edu>
2017-02-09 22:13:26 -0800

    Add methods that allows overlaying multiple histograms.

--- utils/Plotter.py
+++ utils/Plotter.py
@@ -2,6 +2,7 @@
 import matplotlib
 import matplotlib.pyplot as plt
 
+from itertools import izip
 from matplotlib.backends.backend_pdf import PdfPages
 
 class Plotter(object):
@@ -9,7 +10,7 @@ class Plotter(object):
     def __init__(self, file_path): 
         
         plt.style.use('bmh')
-        matplotlib.rcParams.update({'font.size': 12})
+        matplotlib.rcParams.update({'font.size': 20})
         matplotlib.rcParams['axes.facecolor'] = 'white'
         matplotlib.rcParams['legend.numpoints'] = 1
         
@@ -17,23 +18,117 @@ class Plotter(object):
 
     def plot_hist(self, values, bins, **params):
         
-        fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(10, 10))
+        fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(15, 10))
         
         label=None
         if 'label' in params:
             label=params['label']
             ax.legend()
-        
+       
+        norm = False
+        if 'norm' in params: 
+            norm = True
+
         if 'ylog' in params:
-            ax.set_yscale('symlog')
+            if norm: ax.set_yscale('log')
+            else: ax.set_yscale('symlog')
 
         if 'x_label' in params:
             ax.set_xlabel(params['x_label'])
 
-        ax.hist(values, bins, histtype='step', lw=1.5, label=label)
+
+        ax.hist(values, bins, histtype='step', lw=1.5, label=label, normed=norm)
 
         self.pdf.savefig(bbox_inches='tight')
         plt.close()
+
+    def plot_hists(self, values, bins, **params):
+
+        fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(15, 10))
+      
+        label=None
+
+        norm = False
+        if 'norm' in params: 
+            norm = True
+
+        if 'ylog' in params:
+            if norm: ax.set_yscale('log')
+            else: ax.set_yscale('symlog')
+
+        if 'x_label' in params:
+            ax.set_xlabel(params['x_label'])
+
+        labels = None
+        if 'labels' in params:
+            labels = params['labels']
+ 
+        for x_arr, label in izip(values, labels):
+            ax.hist(x_arr, bins, histtype='step', lw=1.5, normed=norm, label=label)
+
+        if labels: ax.legend()
+
+        self.pdf.savefig(bbox_inches='tight')
+        plt.close()
+
+    def plot_graph(self, x, y, x_err, y_err, **params):
+       
+        fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(15, 10))
+        
+        label=None
+        if 'label' in params:
+            label=params['label']
+            ax.legend()
+       
+        if 'x_label' in params:
+            ax.set_xlabel(params['x_label'])
+
+        if 'y_label' in params:
+            ax.set_ylabel(params['y_label'])
+
+        if 'ylim' in params: 
+            ax.set_ylim(params['ylim'])
+
+        if 'xlog' in params:
+            ax.set_xscale('symlog')
+
+        ax.errorbar(x, y, x_err, y_err, markersize=10, marker='o', 
+                    linestyle='-', fmt='', label=label)
+
+        self.pdf.savefig(bbox_inches='tight')
+        plt.close()
+
+
+    def plot_graphs(self, x, y, x_err, y_err, **params):
+       
+        fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(15, 10))
+        
+        labels=None
+        if 'labels' in params:
+            labels=params['labels']
+       
+        if 'x_label' in params:
+            ax.set_xlabel(params['x_label'])
+
+        if 'y_label' in params:
+            ax.set_ylabel(params['y_label'])
+
+        if 'ylim' in params: 
+            ax.set_ylim(params['ylim'])
+        
+        if 'xlog' in params:
+            ax.set_xscale('symlog')
+
+        for index in xrange(0, len(x)):
+            ax.errorbar(x[index], y[index], x_err, y_err, 
+                        markersize=6, marker='o', 
+                        linestyle='-', fmt='', label=labels[index])
+
+        if labels: ax.legend()
+        
+        self.pdf.savefig(bbox_inches='tight')
+        plt.close()
+
 
     def close(self):
         self.pdf.close()
