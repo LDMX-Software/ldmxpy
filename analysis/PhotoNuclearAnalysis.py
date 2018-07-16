d88b7f29ed1b9fda46335894a83f43af3ebdc3f5
omar-moreno <email@omarmoreno.net>
2018-07-16 10:44:40 -0700

    Cleanup.

--- analysis/PhotoNuclearAnalysis.py
+++ analysis/PhotoNuclearAnalysis.py
@@ -12,8 +12,6 @@ class PhotoNuclearAnalysis(object):
     def __init__(self): 
    
         self.tree = None
-        # Open the ROOT file that will be used to write the ntuple to.
-        #self.file = root_open('pn_ntuple.root', 'recreate')
 
     def initialize(self): 
         self.tree = Tree('pn_ntuple', model=PhotoNuclearEvent)
@@ -24,6 +22,9 @@ class PhotoNuclearAnalysis(object):
     def process(self, event):
 
         self.event_count += 1
+        #print "################################################\n"
+        #print "#                   Event %s                   #\n" % self.event_count
+        #print "################################################\n"
 
         # Get the collection of MC particles from the event.
         particles = event.get_collection('SimParticles_sim')
@@ -46,73 +47,52 @@ class PhotoNuclearAnalysis(object):
         self.tree.pn_gamma_vertex_z   = pn_gamma.getVertex()[2]
 
         # Get the lead hadron in the event
-        hadron = self.get_lead_hadron(pn_gamma)
-        self.tree.lead_hadron_ke = au.get_kinetic_energy(hadron)
-        self.tree.lead_hadron_pdg_id = hadron.getPdgID()
-        self.tree.lead_hadron_theta_z = au.get_theta_z(hadron) 
+        lead_hadron, lead_proton, lead_neutron, lead_pion = self.get_lead_hadrons(pn_gamma)
+        self.tree.lead_hadron_ke = au.get_kinetic_energy(lead_hadron)
+        self.tree.lead_hadron_pdg_id = lead_hadron.getPdgID()
+        self.tree.lead_hadron_theta_z = au.get_theta_z(lead_hadron) 
 
-        # Get the PN weight from the event
-        weights = event.get_collection('PNweight_recon') 
-        pn_weight = float(weights[0].getWeight())
-        self.tree.pn_weight = pn_weight
+        if lead_neutron:
+            self.tree.lead_neutron_ke = au.get_kinetic_energy(lead_neutron)
+            self.tree.lead_neutron_theta_z = au.get_theta_z(lead_neutron)
 
-        # Get the FindableTracks collection from the event
-        findable_tracks = event.get_collection('FindableTracks_recon')
-        findable_dic, stub_dic, axial_dic = au.get_findable_tracks_map(findable_tracks)
+        if lead_proton: 
+            self.tree.lead_proton_ke = au.get_kinetic_energy(lead_proton)
+            self.tree.lead_proton_theta_z = au.get_theta_z(lead_proton)
 
-        recoil_hits = event.get_collection('SiStripHits_recon')
-        if len(findable_dic) == 1: 
-           
-            #print 'Event has single track'
+        if lead_pion: 
+            self.tree.lead_pion_ke = au.get_kinetic_energy(lead_pion)
+            self.tree.lead_pion_theta_z = au.get_theta_z(lead_pion)
 
-            findable_track = findable_dic.itervalues().next()
-            sim_particle = findable_track.getSimParticle()
-            self.tree.single_trk_pdg = findable_track.getSimParticle().getPdgID()
+        # If the collection of PN weights exist, retrieve it from the event. 
+        if event.collection_exist('PNweight_recon'): 
+            weights = event.get_collection('PNweight_recon')
             
-            if sim_particle == recoil_e:
-                p_max = 0
-                pt_max = 0
-                for recoil_hit in recoil_hits: 
-                    recoil_sim_hit = recoil_hit.getSimTrackerHits().At(0)
-                    if recoil_sim_hit.getLayerID() == 1: 
-                        if recoil_sim_hit.getSimParticle() == recoil_e:
-                            p = la.norm(recoil_sim_hit.getMomentum())
-                            if p_max < p: 
-                                p_max = p
-                                pt_max = au.get_pt(recoil_sim_hit.getMomentum())
-                    
-                p_find = p_max
-                pt_find = pt_max
-                print 'Single track momentum: %s' % p_find
-                print 'Single track momentum: %s' % pt_find
-            else: 
-                p_find = la.norm(sim_particle.getMomentum())
-                pt_find = au.get_pt(sim_particle.getMomentum())
-            
-            self.tree.single_trk_p = p_find
-            self.tree.single_trk_pt = pt_find
-        
-        #events = [3, 6, 73, 92, 109, 113, 121, 124, 129, 134, 145, 146, 205,
-        #          208, 215, 238, 253, 290, 307, 335, 344, 355, 359, 366, 396,
-        #          397, 419, 428, 442, 463, 505, 534, 560, 567, 587, 591, 625, 637]
+            # Retrieve the PN weight from the event
+            pn_weight = float(weights[0].getWeight())
+            self.tree.pn_weight = pn_weight
+    
+        # Determine if an event can be classified as a single neutron event   
+        fneutron_count, fproton_count = self.hard_forward_hadron_count(pn_gamma)
+        if fneutron_count == 1: 
+            self.tree.is_single_neutron = 1
+        elif fneutron_count == 2: 
+            self.tree.is_dineutron = 1
+        elif fproton_count == 2: 
+            self.tree.is_diproton = 1
+        elif (fneutron_count == 1) & (fproton_count == 1): 
+            self.tree.is_pn = 1
 
-        #events = [36, 57, 83]
-        if self.event_count in events: 
-            self.tfile.write("################################################\n")
-            self.tfile.write("#                   Event %s                   #\n" % self.event_count)
-            self.tfile.write("################################################\n")
-            for pn_daughter_count in xrange(0, pn_gamma.getDaughterCount()): 
-                pn_daughter = pn_gamma.getDaughter(pn_daughter_count)
-            
-                ke = pn_daughter.getEnergy() - pn_daughter.getMass()
-                pvec = pn_daughter.getMomentum()
-                p = la.norm(pvec)
-                theta = math.acos(pvec[2]/p)*180.0/3.14159
-                ep = pn_daughter.getEndPoint()
+        if self.is_ghost_event(lead_hadron):
+            #print 'Is ghost'
+            self.tree.is_ghost = 1
 
-                self.tfile.write('\tPDG ID: %s, KE: %s, theta: %s, p: %s, end point: [%s, %s, %s] \n' % (
-                    pn_daughter.getPdgID(), ke, theta, p, ep[0], ep[1], ep[2]))
-            
+        # If the collection of findable tracks exist, retrieve it from the 
+        # event. 
+        #if event.collection_exist('FindableTracks_recon'): 
+
+        #    findable_tracks = event.get_collection('FindableTracks_recon')
+        #    findable_dic, stub_dic, axial_dic = au.get_findable_tracks_map(findable_tracks)    
 
         self.tree.fill(reset=True)
 
@@ -121,13 +101,18 @@ class PhotoNuclearAnalysis(object):
         self.tree.write()
         self.tfile.close()
 
-    def get_lead_hadron(self, pn_gamma): 
+    def get_lead_hadrons(self, pn_gamma): 
 
-        lead_ke = -9999
-        lead_proton = -9999
-        lead_neutron = -9999
-        lead_pion = -9999
-        lead_hadron = None
+        lead_ke         = -9999
+        lead_proton_ke  = -9999
+        lead_neutron_ke = -9999
+        lead_pion_ke    = -9999
+
+        lead_hadron  = None
+        lead_proton  = None
+        lead_neutron = None
+        lead_pion    = None
+        
         for pn_daughter_count in xrange(0, pn_gamma.getDaughterCount()): 
             pn_daughter = pn_gamma.getDaughter(pn_daughter_count)
             
@@ -136,6 +121,58 @@ class PhotoNuclearAnalysis(object):
                 lead_ke = ke
                 lead_hadron = pn_daughter
 
-        return lead_hadron
+            if pn_daughter.getPdgID() == 2112: 
+                if lead_neutron_ke < ke: 
+                    lead_neutron_ke = ke
+                    lead_neutron = pn_daughter
+            
+            if pn_daughter.getPdgID() == 2212: 
+                if lead_proton_ke < ke: 
+                    lead_proton_ke = ke
+                    lead_proton = pn_daughter
+            
+            if (abs(pn_daughter.getPdgID()) == 211) or (pn_daughter.getPdgID() == 111):
+                if lead_pion_ke < ke: 
+                    lead_pion_ke = ke
+                    lead_pion = pn_daughter
 
+        return lead_hadron, lead_proton, lead_neutron, lead_pion
 
+    def hard_forward_hadron_count(self, pn_gamma): 
+
+        #lh, lp, ln, lp = self.get_lead_hadrons(pn_gamma)
+
+        fneutron_count = 0
+        fproton_count = 0
+
+        for pn_daughter_count in xrange(0, pn_gamma.getDaughterCount()): 
+            pn_daughter = pn_gamma.getDaughter(pn_daughter_count)
+            #if pn_daughter == ln: continue
+
+            ke = au.get_kinetic_energy(pn_daughter)
+            theta_z = au.get_theta_z(pn_daughter)
+            if ke > 100 and theta_z < 90:
+                if pn_daughter.getPdgID() == 2112: 
+                    fneutron_count += 1
+                elif pn_daughter.getPdgID() == 2212: 
+                    fproton_count += 1
+        
+        return fneutron_count, fproton_count
+
+    def is_ghost_event(self, lead_hadron): 
+        
+        ke = au.get_kinetic_energy(lead_hadron)
+        theta_z = au.get_theta_z(lead_hadron)
+
+        #if theta_z > 80: return 1
+        #return 0
+        theta_z_theo = math.exp(-0.000575249*ke + 5.3643)
+        
+        #print 'KE: %s, Theta: %s, Theta (exp): %s' % (ke, theta_z, theta_z_theo)
+        
+        if theta_z > theta_z_theo: 
+            #print 'Ghost event found!'
+            return 1
+        else: return 0
+       
+        
