3f1f9b249408bde9a7194de5aef7f7b52f4cca50
omar-moreno <email@omarmoreno.net>
2018-08-14 23:44:52 -0700

    Add more variables.

--- analysis/EcalAnalysis.py
+++ analysis/EcalAnalysis.py
@@ -1,10 +1,10 @@
 
+from __future__ import division
 
 from numpy import linalg as la
 from rootpy.tree import Tree
 
 import AnalysisUtils as au
-#import Line as l
 
 from EventModels import EcalEvent
 
@@ -18,97 +18,110 @@ class EcalAnalysis(object):
 
         self.tree = Tree('ecal_ntuple', model=EcalEvent)
 
-        #self.tfile = open('events.log', 'w')
-
-        #self.event_count = 0
-        #self.cfile_name = ''
-
     def process(self, event):
     
-        #if self.cfile_name != event.get_file_name(): 
-        #    self.event_count = 0
-        #    self.cfile_name = event.get_file_name()
-        
-        #self.event_count += 1
-
-        # Get the collection of MC particles from the event
-        particles = event.get_collection('SimParticles_sim')
-
-        # Search the list of sim particles for the recoil electron.  If it isn't
-        # found, throw an exception.
-        recoil_e = au.get_recoil_electron(particles)
-
-        # Get the collection of EcalVeto objects from the event.  These objects
-        # contain the results from the BDT.
-        ecal_veto_results = event.get_collection('EcalVeto_recon')
-
-        # Get the BDT probability.
-        self.tree.bdt_prob = ecal_veto_results[0].getDisc()
-        self.tree.passes_ecal_veto =  0
-        if ecal_veto_results[0].passesVeto(): self.tree.passes_ecal_veto =  1
-
-        # Get the scoring plane hits at the Ecal and search for any hits
-        # associted with the recoil electron
-        ecal_sp_hits = event.get_collection('EcalScoringPlaneHits_sim')
-        self.tree.recoil_e_ecal_sp_x = -9999
-        self.tree.recoil_e_ecal_sp_y = -9999
-        self.tree.recoil_e_ecal_sp_z = -9999
-        max_p = 0
-        recoil_e_ecal_sp_hit = None
-        for ecal_sp_hit in ecal_sp_hits: 
-            if ecal_sp_hit.getSimParticle() == recoil_e:
-                if ecal_sp_hit.getMomentum()[2] <= 0: continue
-                #print 'Ecal scoring plane: x: %s y: %s z: %s ' % (ecal_sp_hit.getPosition()[0],
-                #        ecal_sp_hit.getPosition()[1], ecal_sp_hit.getPosition()[2])
-                #print 'Ecal scoring plane: px: %s py: %s pz: %s' % (ecal_sp_hit.getMomentum()[0], 
-                #        ecal_sp_hit.getMomentum()[1], ecal_sp_hit.getMomentum()[2])
-                pvec = ecal_sp_hit.getMomentum()
-                p = la.norm(pvec)
-                if max_p < p: 
-                    #print 'Max p found'
-                    max_p = p
-                    recoil_e_ecal_sp_hit = ecal_sp_hit
-       
-        #line = l.Line(recoil_e_ecal_sp_hit.getPosition(), recoil_e_ecal_sp_hit.getMomentum())
-
-        #if self.tree.recoil_e_ecap_sp_x != -9999: 
-            
-
         #
         # Hit Level
         #
         
         # Get the collection of digitized Ecal hits
         ecal_dhits = event.get_collection('ecalDigis_recon')
-      
-        ecal_dhit_count = 0
-        ecal_dhit_l1_count = 0
-        ecal_dhit_l1_energy_sum = 0
-        denergy_sum = 0
+
+        # counters
+        layer_sum = 0
+        max_layer_hit = 0
+        max_cell_energy = 0
+        ecal_hit_x = []
+        ecal_hit_y = []
 
         for ecal_dhit in ecal_dhits: 
-
+        
             # Get the digitized hit energy
             denergy = ecal_dhit.getEnergy()
-
+            
             # Currently, if the energy of a digitized hit is below threshold,
             # its energy is set to zero.  These hits aren't considered.
             if denergy == 0: continue 
-            ecal_dhit_count += 1
-            
-            if ecal_dhit.getLayer() == 1:
-                if ecal_dhit.getAmplitude() > 5*self.get_noise_rms():
-                    ecal_dhit_l1_count += 1
-                    ecal_dhit_l1_energy_sum += denergy
+            self.tree.ecal_dhit_count += 1
 
-            denergy_sum += denergy
+            layer = ecal_dhit.getLayer()
+            self.tree.ecal_dhit_layer.push_back(layer)
+            layer_sum += layer
+            max_layer_hit = max(max_layer_hit, layer)
+
+            self.tree.total_ecal_denergy += denergy
+            self.tree.ecal_dhit_energy.push_back(denergy)
+            max_cell_energy = max(max_cell_energy, denergy)
+
+            if layer == 1:
+                self.tree.ecal_layer1_hit_count += 1
+                self.tree.ecal_layer1_energy_sum += denergy
+
+            if layer <= 20: 
+                self.tree.trigger_energy_sum += denergy
+        
+        if self.tree.ecal_dhit_count > 0: 
+            self.tree.average_ecal_layer_hit = layer_sum/self.tree.ecal_dhit_count
+        
+        self.tree.ecal_max_layer_hit = max_layer_hit
+        self.tree.ecal_max_denergy_cell = max_cell_energy
+        
+        # Get the collection of Ecal veto results from the event
+        ecal_veto_results = event.get_collection('EcalVeto_recon')
        
-            #print "Distance to: %s" % line.distance_to_point(ecal_dhit.getPosition())
+        self.tree.vecal_max_denergy_cell = ecal_veto_results[0].getMaxCellDep()
+        self.tree.vtotal_ecal_denergy    = ecal_veto_results[0].getSummedDet()
+        self.tree.vecal_max_layer_hit    = ecal_veto_results[0].getDeepestLayerHit()
+        self.tree.vecal_summed_tight_iso = ecal_veto_results[0].getSummedTightIso()
+        self.tree.vecal_shower_rms = ecal_veto_results[0].getShowerRMS()
+        self.tree.vecal_x_pos_std = ecal_veto_results[0].getXStd()
+        self.tree.vecal_y_pos_std = ecal_veto_results[0].getYStd()
+        self.tree.vecal_layer_std = ecal_veto_results[0].getStdLayerHit()
+        self.tree.vaverage_ecal_layer_hit = ecal_veto_results[0].getAvgLayerHit()
+       
+        # Check if the ECal veto collection exist. If it does, use it to get the
+        # BDT result. 
+        if event.collection_exist('EcalVeto_recon'): 
+            
+            # Get the collection of BDT results from the event.
+            ecal_veto_results = event.get_collection('EcalVeto_recon')
+            
+            # Get the BDT probability.
+            self.tree.bdt_prob = ecal_veto_results[0].getDisc()
+            
+            # Check if the veto passed.
+            if ecal_veto_results[0].passesVeto(): self.tree.passes_ecal_veto =  1
 
-        self.tree.total_ecal_denergy = denergy_sum
-        self.tree.ecal_layer1_hit_count = ecal_dhit_l1_count
-        self.tree.ecal_layer1_energy_sum = ecal_dhit_l1_energy_sum
         '''
+
+        # Get the collection of MC particles from the event
+        particles = event.get_collection('SimParticles_sim')
+
+        # Search the list of sim particles for the recoil electron.  If it isn't
+        # found, throw an exception.
+        #recoil_e = au.get_recoil_electron(particles)
+
+        # Get the scoring plane hits at the Ecal and search for any hits
+        # associted with the recoil electron
+        #ecal_sp_hits = event.get_collection('EcalScoringPlaneHits_sim')
+        #self.tree.recoil_e_ecal_sp_x = -9999
+        #self.tree.recoil_e_ecal_sp_y = -9999
+        #self.tree.recoil_e_ecal_sp_z = -9999
+        #max_p = 0
+        #recoil_e_ecal_sp_hit = None
+        #for ecal_sp_hit in ecal_sp_hits: 
+        #    if ecal_sp_hit.getSimParticle() == recoil_e:
+        #        if ecal_sp_hit.getMomentum()[2] <= 0: continue
+                #print 'Ecal scoring plane: x: %s y: %s z: %s ' % (ecal_sp_hit.getPosition()[0],
+                #        ecal_sp_hit.getPosition()[1], ecal_sp_hit.getPosition()[2])
+                #print 'Ecal scoring plane: px: %s py: %s pz: %s' % (ecal_sp_hit.getMomentum()[0], 
+                #        ecal_sp_hit.getMomentum()[1], ecal_sp_hit.getMomentum()[2])
+        #        pvec = ecal_sp_hit.getMomentum()
+        #        p = la.norm(pvec)
+        #        if max_p < p: 
+                    #print 'Max p found'
+        #            max_p = p
+        #            recoil_e_ecal_sp_hit = ecal_sp_hit
 
         if recoil_e_ecal_sp_hit: 
             self.tree.recoil_e_ecal_sp_x = recoil_e_ecal_sp_hit.getPosition()[0]
@@ -120,33 +133,12 @@ class EcalAnalysis(object):
             self.tree.recoil_e_ecal_sp_py = recoil_e_ecal_sp_hit.getMomentum()[1]
             self.tree.recoil_e_ecal_sp_pz = recoil_e_ecal_sp_hit.getMomentum()[2]
         '''
-        ''' 
-        if self.tree.bdt_prob > 0: 
-            self.tfile.write('%s : %s : %s : %s\n' % (event.get_file_name(),
-                                                 event.get_event_number(),
-                                                 self.event_count, 
-                                                 pn_weight))
-            
-            for pn_daughter_count in xrange(0, pn_gamma.getDaughterCount()): 
-                pn_daughter = pn_gamma.getDaughter(pn_daughter_count)
-            
-                ke = pn_daughter.getEnergy() - pn_daughter.getMass()
-                pvec = pn_daughter.getMomentum()
-                p = la.norm(pvec)
-                theta = math.acos(pvec[2]/p)*180.0/3.14159
-                ep = pn_daughter.getEndPoint()
-
-                self.tfile.write('\tPDG ID: %s, KE: %s, theta: %s, p: %s, end point: [%s, %s, %s] \n' % (
-                    pn_daughter.getPdgID(), ke, theta, p, ep[0], ep[1], ep[2]))
-
-        '''
         
         self.tree.fill(reset=True)
 
     def finalize(self): 
 
         self.tree.write()
-        #self.tfile.close()
 
     def get_noise_rms(self):
         return (900 + 22*27.56)*(0.130/33000)
